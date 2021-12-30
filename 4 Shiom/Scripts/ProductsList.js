9378cce80bd15bc0948ec7c6f8b057d8c39e82b2
shiom05 <60684682+shiom05@users.noreply.github.com>
2021-12-31 01:04:44 +0530

    Products List page

--- 4 Shiom/Scripts/ProductsList.js
+++ 4 Shiom/Scripts/ProductsList.js
@@ -29,7 +29,7 @@ var Products =
     "product_id": "3",
     "title": "Kraft Cheddar Cheese",
     "category": "Dairy",
-    "description": "Soft cheddar cheese",
+    "description": "Neque porro quisquam est qui dolore ipsum quia dolor sit amet.",
     "price": 3.50,
     "rating": 4.9,
     "img_url": "Images/cheese.png",
@@ -50,7 +50,9 @@ var Products =
     "brand":"Nestle",
   },
   
-  ]
+  ];
+
+  var tempProductHolder = [];
 
   $(document).ready(function () {
 
@@ -70,6 +72,165 @@ var Products =
         for (var i = 0; i < Products.length; i++) {
 
             if(filterVal==Products[i].brand){
+
+                outputFilter += `
+        <div class="card">
+                    <div class="cardheader">
+
+                        <div class="cardHeart">
+                            <div class="addToWishL">
+                                <i id="wishIcon" class="fa fa-heart-o" aria-hidden="true"></i>
+                            </div>
+                            <div class="discounted">
+                               <img  id="discountTag" src="" alt="">
+                            </div>
+                        </div>
+                        <div class="cardImage">
+                            <img id="product-image" class="card-img-top" src="${Products[i].img_url}" alt="Card image cap">
+                        </div>
+
+                    </div>
+                   
+                    <div class="card-body">
+
+                        <div class="product_title">
+                            <h5 id="productTitle" class="card-title">${Products[i].title}</h5>
+                        </div>
+
+                        <div class="product_sub_info">
+
+                            <div class="product_type">
+                             <p id="product_cat">${Products[i].category}</p>
+                            </div>
+
+                            <div class="product_rating">
+                            <p id="procustRating"> <span>&#9733;</span> ${Products[i].rating}</p>
+                            </div>
+
+                        </div>
+
+                        <div class="product_description">
+                            <p id="productDescription" class="card-text">${Products[i].description}</p>
+                        </div>
+
+                        <div class="product_price">
+                               
+                               <p id="productPrice"> <span id="beforeDiscounted">${Products[i].beforeDiscount}</span> <span id="price_product"> ${'$'+Products[i].price}</span></p>
+                        </div>
+
+                        <div class="product_footer">
+
+                            <div class="QuanitityContainer">
+
+                                <button type="button" id="plusButton" class="btn btn-secondary">+</button>
+                                <span id="quantity">1</span>
+                               <button type="button" id="minusButton" class="btn btn-secondary">-</button>
+                            </div>
+
+                            <div class="addToCartContainer">
+                                <div class="continue">
+                                    <p>Add to cart</p>
+                                   </div>
+                            </div>  
+                        </div>                    
+
+                    </div>
+                </div>
+      `;
+            }
+
+
+          if(filterVal=="All"){
+              
+            outputFilter += `
+            <div class="card">
+                        <div class="cardheader">
+    
+                            <div class="cardHeart">
+                                <div class="addToWishL">
+                                <span id="wishIcon">&#9825;</i>
+                                </div>
+                                <div class="discounted">
+                                   <img  id="discountTag" src="" alt="">
+                                </div>
+                            </div>
+                            <div class="cardImage">
+                                <img id="product-image" class="card-img-top" src="${Products[i].img_url}" alt="Card image cap">
+                            </div>
+    
+                        </div>
+                       
+                        <div class="card-body">
+    
+                            <div class="product_title">
+                                <h5 id="productTitle" class="card-title">${Products[i].title}</h5>
+                            </div>
+    
+                            <div class="product_sub_info">
+    
+                                <div class="product_type">
+                                <p id="procustRating"> <span>&#9733;</span> ${Products[i].rating}</p>
+                                </div>
+    
+                                <div class="product_rating">
+                                  <p id="procustRating">${Products[i].rating}</p>
+                                </div>
+    
+                            </div>
+    
+                            <div class="product_description">
+                                <p id="productDescription" class="card-text">${Products[i].description}</p>
+                            </div>
+    
+                            <div class="product_price">
+                                   
+                                   <p id="productPrice"> <span id="beforeDiscounted">${Products[i].beforeDiscount}</span> <span id="price_product"> ${'$'+Products[i].price}</span></p>
+                            </div>
+    
+                            <div class="product_footer">
+    
+                                <div class="QuanitityContainer">
+    
+                                    <button type="button" id="plusButton" class="btn btn-secondary">+</button>
+                                    <span id="quantity">1</span>
+                                   <button type="button" id="minusButton" class="btn btn-secondary">-</button>
+                                </div>
+    
+                                <div class="addToCartContainer">
+                                    <div class="continue">
+                                        <p>Add to cart</p>
+                                       </div>
+                                </div>  
+                            </div>                    
+    
+                        </div>
+                    </div>
+          `;
+
+
+          }  
+
+        }
+
+        container.innerHTML=outputFilter;  
+
+    }); //End of Filter by Brand
+
+
+    $('#SortBy').on('change', function () {
+
+        const container = document.getElementById('product_List');
+        container.innerHTML='';
+
+        var outputFilter = '';
+        var $this = $(this),
+        filterVal   = $this.val();
+      
+        for (i = 0; i < Products.length; i++) {
+            tempProductHolder[i] = Products[i];
+          }
+
+            if(filterVal=='Best'){
 
                 outputFilter += `
         <div class="card">
@@ -136,17 +297,191 @@ var Products =
                 </div>
       `;
             }
+            
+            else if(filterVal=='popularity'){
+
+                for(var x=0; x<tempProductHolder.length; x++){
+
+                    for(var j=x+1; j<tempProductHolder.length; j++){
+
+                        if(tempProductHolder[x].rating<tempProductHolder[j].rating){
+                            var tempObj = tempProductHolder[j];
+                            tempProductHolder[j]=tempProductHolder[x];
+                            tempProductHolder[x]=tempObj;  
+                        }
+                    }
+                }
+
+                for (var i = 0; i < tempProductHolder.length; i++) {
+                    outputFilter += `
+                    <div class="card">
+                                <div class="cardheader">
+            
+                                    <div class="cardHeart">
+                                        <div class="addToWishL">
+                                        <span id="wishIcon">&#9825;</i>
+                                        </div>
+                                        <div class="discounted">
+                                           <img  id="discountTag" src="" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="cardImage">
+                                        <img id="product-image" class="card-img-top" src="${tempProductHolder[i].img_url}" alt="Card image cap">
+                                    </div>
+            
+                                </div>
+                               
+                                <div class="card-body">
+            
+                                    <div class="product_title">
+                                        <h5 id="productTitle" class="card-title">${tempProductHolder[i].title}</h5>
+                                    </div>
+            
+                                    <div class="product_sub_info">
+            
+                                        <div class="product_type">
+                                         <p id="product_cat">${tempProductHolder[i].category}</p>
+                                        </div>
+            
+                                        <div class="product_rating">
+                                        <p id="procustRating"> <span>&#9733;</span> ${tempProductHolder[i].rating}</p>
+                                        </div>
+            
+                                    </div>
+            
+                                    <div class="product_description">
+                                        <p id="productDescription" class="card-text">${tempProductHolder[i].description}</p>
+                                    </div>
+            
+                                    <div class="product_price">
+                                           
+                                           <p id="productPrice"> <span id="beforeDiscounted">${tempProductHolder[i].beforeDiscount}</span> <span id="price_product"> ${'$'+tempProductHolder[i].price}</span></p>
+                                    </div>
+            
+                                    <div class="product_footer">
+            
+                                        <div class="QuanitityContainer">
+            
+                                            <button type="button" id="plusButton" class="btn btn-secondary">+</button>
+                                            <span id="quantity">1</span>
+                                           <button type="button" id="minusButton" class="btn btn-secondary">-</button>
+                                        </div>
+            
+                                        <div class="addToCartContainer">
+                                            <div class="continue">
+                                                <p>Add to cart</p>
+                                               </div>
+                                        </div>  
+                                    </div>                    
+            
+                                </div>
+                            </div>
+                  `;
+                }
+
+
+
+
+            }
+            
+            else if(filterVal == 'alphabetical'){
+
+                for(var x=0; x<tempProductHolder.length; x++){
+
+                    for(var j=x+1; j<tempProductHolder.length; j++){
+
+                        if(tempProductHolder[x].title>tempProductHolder[j].title){
+                            var tempObj = tempProductHolder[j];
+                            tempProductHolder[j]=tempProductHolder[x];
+                            tempProductHolder[x]=tempObj;  
+                        }
+                    }
+                } 
+             
+                for (var i = 0; i < tempProductHolder.length; i++) {
+                    outputFilter += `
+                    <div class="card">
+                                <div class="cardheader">
+            
+                                    <div class="cardHeart">
+                                        <div class="addToWishL">
+                                        <span id="wishIcon">&#9825;</i>
+                                        </div>
+                                        <div class="discounted">
+                                           <img  id="discountTag" src="" alt="">
+                                        </div>
+                                    </div>
+                                    <div class="cardImage">
+                                        <img id="product-image" class="card-img-top" src="${tempProductHolder[i].img_url}" alt="Card image cap">
+                                    </div>
+            
+                                </div>
+                               
+                                <div class="card-body">
+            
+                                    <div class="product_title">
+                                        <h5 id="productTitle" class="card-title">${tempProductHolder[i].title}</h5>
+                                    </div>
+            
+                                    <div class="product_sub_info">
+            
+                                        <div class="product_type">
+                                         <p id="product_cat">${tempProductHolder[i].category}</p>
+                                        </div>
+            
+                                        <div class="product_rating">
+                                        <p id="procustRating"> <span>&#9733;</span> ${tempProductHolder[i].rating}</p>
+                                        </div>
+            
+                                    </div>
+            
+                                    <div class="product_description">
+                                        <p id="productDescription" class="card-text">${tempProductHolder[i].description}</p>
+                                    </div>
+            
+                                    <div class="product_price">
+                                           
+                                           <p id="productPrice"> <span id="beforeDiscounted">${tempProductHolder[i].beforeDiscount}</span> <span id="price_product"> ${'$'+tempProductHolder[i].price}</span></p>
+                                    </div>
+            
+                                    <div class="product_footer">
+            
+                                        <div class="QuanitityContainer">
+            
+                                            <button type="button" id="plusButton" class="btn btn-secondary">+</button>
+                                            <span id="quantity">1</span>
+                                           <button type="button" id="minusButton" class="btn btn-secondary">-</button>
+                                        </div>
+            
+                                        <div class="addToCartContainer">
+                                            <div class="continue">
+                                                <p>Add to cart</p>
+                                               </div>
+                                        </div>  
+                                    </div>                    
+            
+                                </div>
+                            </div>
+                  `;
+                }
+            }
+            
+            else if(filterVal == 'discount'){
+
+            }
 
 
           if(filterVal=="All"){
               
+            for (var i = 0; i < Products.length; i++) {
+
             outputFilter += `
             <div class="card">
                         <div class="cardheader">
     
                             <div class="cardHeart">
                                 <div class="addToWishL">
-                                    <i id="wishIcon" class="fa fa-heart-o" aria-hidden="true"></i>
+                                <span id="wishIcon">&#9825;</i>
                                 </div>
                                 <div class="discounted">
                                    <img  id="discountTag" src="" alt="">
@@ -205,15 +540,16 @@ var Products =
                     </div>
           `;
 
+            }
 
           }  
 
-        }
+        
 
         container.innerHTML=outputFilter;  
 
-    }); //End of Filter by Brand
-
+    });
+    
     $("#addFilter").on('click',function(){
 
 
@@ -231,8 +567,33 @@ var Products =
 
     });
 
+    $(".addToWishL").on('click',function(){
+        
+        console.log($(this).attr('wishIcon'))
 
-  });
+        if($(this).text() == '♡'){
+
+            $(this).html("&#10084;")
+            $(this).css("color", "red");
+            $(this).css("font-size", "30px");
+            $(this).css("margin-left", "5%");
+        }else {
+
+            $(this).css("color", "black");
+            $(this).html("&#9825;")
+            $(this).css("font-size", "35px");
+            $(this).css("margin-left", "5%");
+        }
+       
+        
+
+        
+    });
+
+
+  }); //End of Document ready func
+
+
 
   function populateProductCard() {
 
@@ -250,7 +611,7 @@ var Products =
 
                         <div class="cardHeart">
                             <div class="addToWishL">
-                                <i id="wishIcon" class="fa fa-heart-o" aria-hidden="true"></i>
+                            <span id="wishIcon"  >&#9825;</i>
                             </div>
                             <div class="discounted">
                                <img  id="discountTag" src="" alt="">
@@ -275,7 +636,7 @@ var Products =
                             </div>
 
                             <div class="product_rating">
-                              <p id="procustRating">${Products[i].rating}</p>
+                            <p id="procustRating"> <span>&#9733;</span> ${Products[i].rating}</p>
                             </div>
 
                         </div>
@@ -313,5 +674,7 @@ var Products =
     container.innerHTML=output;  
   }//Populate Method end
 
+
+ 
 
   
