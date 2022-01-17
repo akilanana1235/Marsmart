448081c9846d3f03385a19a2bc6be58415ae636d
shiom05 <60684682+shiom05@users.noreply.github.com>
2022-01-17 09:02:20 +0530

    Minor Changes

--- 4 Shiom/Scripts/LeaderBoard.js
+++ 4 Shiom/Scripts/LeaderBoard.js
@@ -2,26 +2,6 @@ var playerscore = 0;
 $(document).ready(function(){
 
   let loyaltyPoints = localStorage.getItem('UserLoyaltyPoints');
-    
-    // if(!localStorage.getItem("playerscore")){
-    //     playerscore += 0;
-    // }else{
-    //   playerscore += parseInt(localStorage.getItem("playerscore"));
-    // }
-    
-    //  //getting Memmory game score
-    // if(!localStorage.getItem("memoryGameScore")){
-    //   playerscore += 0;
-    // }else{
-    //   playerscore += parseInt(localStorage.getItem("memoryGameScore"));
-    // }
-
-    // if(!localStorage.getItem("spinGameScore")){
-    //   playerscore += 0;
-    // }else{
-    //   playerscore += parseInt(localStorage.getItem("spinGameScore"));
-    // }
-
     document.getElementById("loyaltyscore").innerHTML=loyaltyPoints;
 
 
